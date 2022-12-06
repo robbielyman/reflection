dd11f6aadf4328518d8114d35214e4f328ee4b77
Rylee Lyman <rylee.alanza@gmail.com>
2022-12-05 21:33:15 -0500

    get with the tabstop=2 program

--- formantsea.lua
+++ formantsea.lua
@@ -13,140 +13,140 @@ local Grid = grid.connect()
 MAX_NUM_VOICES = 16
 
 function init()
-    Num_Voices = 0
-    Needs_Restart = false
-    local formanttri_files = {"FormantTriPTR.sc", "FormantTriPTR_scsynth.so"}
-    for _,file in pairs(formanttri_files) do
-        if not util.file_exists(extensions .. "/FormantTriPTR/" .. file) then
-            util.os_capture("mkdir " .. extensions .. "/FormantTriPTR")
-            util.os_capture("cp " .. norns.state.path .. "/ignore/" .. file .. " " .. extensions .. "/FormantTriPTR/" .. file)
-            print("installed " .. file)
-            Needs_Restart = true
-        end
+  Num_Voices = 0
+  Needs_Restart = false
+  local formanttri_files = {"FormantTriPTR.sc", "FormantTriPTR_scsynth.so"}
+  for _,file in pairs(formanttri_files) do
+    if not util.file_exists(extensions .. "/FormantTriPTR/" .. file) then
+      util.os_capture("mkdir " .. extensions .. "/FormantTriPTR")
+      util.os_capture("cp " .. norns.state.path .. "/ignore/" .. file .. " " .. extensions .. "/FormantTriPTR/" .. file)
+      print("installed " .. file)
+      Needs_Restart = true
     end
-    Restart_Message = UI.Message.new{"please restart norns"}
-    Grid.key = grid_key
-    Narcissus = Reflection.new()
-    Narcissus.process = grid_note
+  end
+  Restart_Message = UI.Message.new{"please restart norns"}
+  Grid.key = grid_key
+  Narcissus = Reflection.new()
+  Narcissus.process = grid_note
 
-    params:add{
-        type    = "option",
-        id      = "enc2",
-        name    = "enc2",
-        options = {"slope", "formant", "noise", "cut"}
-    }
-    params:add{
-        type    = "option",
-        id      = "enc3",
-        name    = "enc3",
-        options = {"slope", "formant", "noise", "cut"},
-        default = 2
-    }
-    params:add_separator("FormantSub")
-    if engine.name ~= "None" then
-        FormantSub.params()
-        engine.stopAll()
-        params:bang()
-    end
-    Refresh_Metro = metro.init()
-    Refresh_Metro.event = function()
-        redraw()
-        grid_redraw()
-    end
-    Refresh_Metro:start(1/15)
-    Grid_Presses = {}
-    for i = 1, 16 do
-        Grid_Presses[i] = {}
-    end
+  params:add{
+    type    = "option",
+    id      = "enc2",
+    name    = "enc2",
+    options = {"slope", "formant", "noise", "cut"}
+  }
+  params:add{
+    type    = "option",
+    id      = "enc3",
+    name    = "enc3",
+    options = {"slope", "formant", "noise", "cut"},
+    default = 2
+  }
+  params:add_separator("FormantSub")
+  if engine.name ~= "None" then
+    FormantSub.params()
+    engine.stopAll()
+    params:bang()
+  end
+  Refresh_Metro = metro.init()
+  Refresh_Metro.event = function()
+    redraw()
+    grid_redraw()
+  end
+  Refresh_Metro:start(1/15)
+  Grid_Presses = {}
+  for i = 1, 16 do
+    Grid_Presses[i] = {}
+  end
 end
 
 function grid_key(x, y, z)
-    if x == 1 then
-        if z == 1 then
-            if y == 1 then
-                Narcissus:set_rec(Narcissus.rec == 0 and 1 or 0)
-                if Narcissus.rec == 1 and Narcissus.endpoint == 0 then
-                    Narcissus:start()
-                end
-            elseif y == 2 then
-                if Narcissus.play == 0 then
-                    if Narcissus.endpoint == 0 then
-                        Narcissus:set_rec(1)
-                        Narcissus:start()
-                    else
-                        Narcissus:start()
-                    end
-                else
-                    Narcissus:stop()
-                end
-            elseif y == 3 then
-                Narcissus:set_loop(Narcissus.loop == 0 and 1 or 0)
-            end
+  if x == 1 then
+    if z == 1 then
+      if y == 1 then
+        Narcissus:set_rec(Narcissus.rec == 0 and 1 or 0)
+        if Narcissus.rec == 1 and Narcissus.endpoint == 0 then
+          Narcissus:start()
         end
-    else
-        local event = {
-            id = x + y * 16,
-            x = x,
-            y = y,
-            z = z
-        }
-        Narcissus:watch(event)
-        grid_note(event)
+      elseif y == 2 then
+        if Narcissus.play == 0 then
+          if Narcissus.endpoint == 0 then
+            Narcissus:set_rec(1)
+            Narcissus:start()
+          else
+            Narcissus:start()
+          end
+        else
+          Narcissus:stop()
+        end
+      elseif y == 3 then
+        Narcissus:set_loop(Narcissus.loop == 0 and 1 or 0)
+      end
     end
-    Grid_Presses[x][y] = z
+  else
+    local event = {
+      id = x + y * 16,
+      x = x,
+      y = y,
+      z = z
+    }
+    Narcissus:watch(event)
+    grid_note(event)
+  end
+  Grid_Presses[x][y] = z
 end
 
 function grid_note(event)
-    local note = ((10 - event.y)*5) + event.x + 30
-    if event.z == 1 then
-        if Num_Voices < MAX_NUM_VOICES then
-            start_note(event.id, note)
-            Grid_Presses[event.x][event.y] = event.z
-            Num_Voices = Num_Voices + 1
-        end
-    else
-        engine.stop(event.id)
-        Grid_Presses[event.x][event.y] = event.z
-        Num_Voices = Num_Voices - 1
+  local note = ((10 - event.y)*5) + event.x + 30
+  if event.z == 1 then
+    if Num_Voices < MAX_NUM_VOICES then
+      start_note(event.id, note)
+      Grid_Presses[event.x][event.y] = event.z
+      Num_Voices = Num_Voices + 1
     end
+  else
+    engine.stop(event.id)
+    Grid_Presses[event.x][event.y] = event.z
+    Num_Voices = Num_Voices - 1
+  end
 end
 
 function start_note(id, note)
-    if params:get("output") == 1 then
-        engine.start(id, Musicutil.note_num_to_freq(note))
-    elseif params:get("output") == 2  then
-        crow.output[1].volts = note/12
-        crow.output[2].execute()
-    elseif params:get("output") == 3 then
-        crow.ii.jf.play_note(note/12,5)
-    end
+  if params:get("output") == 1 then
+    engine.start(id, Musicutil.note_num_to_freq(note))
+  elseif params:get("output") == 2  then
+    crow.output[1].volts = note/12
+    crow.output[2].execute()
+  elseif params:get("output") == 3 then
+    crow.ii.jf.play_note(note/12,5)
+  end
 end
 
 function grid_redraw()
-    Grid:all(0)
-    Grid:led(1,1,Narcissus.rec == 0 and 0 or 10)
-    Grid:led(1,2,Narcissus.play == 0 and 0 or 10)
-    Grid:led(1,3,Narcissus.loop == 0 and 0 or 10)
+  Grid:all(0)
+  Grid:led(1,1,Narcissus.rec == 0 and 0 or 10)
+  Grid:led(1,2,Narcissus.play == 0 and 0 or 10)
+  Grid:led(1,3,Narcissus.loop == 0 and 0 or 10)
 
-    for i = 1, 16 do
-        for j = 1,8 do
-            if Grid_Presses[i][j] == 1 then
-                Grid:led(i,j,15)
-            end
-        end
+  for i = 1, 16 do
+    for j = 1,8 do
+      if Grid_Presses[i][j] == 1 then
+        Grid:led(i,j,15)
+      end
     end
-    Grid:refresh()
+  end
+  Grid:refresh()
 end
 
 function redraw()
-    screen.clear()
-    if Needs_Restart then
-        Restart_Message:redraw()
-    end
-    screen.update()
+  screen.clear()
+  if Needs_Restart then
+    Restart_Message:redraw()
+  end
+  screen.update()
 end
 
 function cleanup()
-    Narcissus:stop()
-    Narcissus = nil
+  Narcissus:stop()
+  Narcissus = nil
 end
